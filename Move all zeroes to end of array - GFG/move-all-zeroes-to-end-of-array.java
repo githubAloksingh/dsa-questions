b6da34e3b2e16e192d87f1c810f9a121a9fd23a7
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2023-06-17 16:53:04 +0530

    Update move-all-zeroes-to-end-of-array.java

--- Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.java
+++ Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.java
@@ -35,21 +35,22 @@ public class Main {
 class Solution {
     void pushZerosToEnd(int[] arr, int n) {
         // code here
-           int temp[]=new int[n];
-        int j=0;
+    
+
+//User function Template for Java
+
+class Solution {
+    void pushZerosToEnd(int[] arr, int n) {
+        // code here
+        int count=0;
         for(int i=0;i<n;i++){
-         if(arr[i]>0 ||arr[i]<0){
-          temp[j++]=arr[i];
-         }}
-    for(int i=0;i<n;i++){
-          if(arr[i]==0){
-          temp[j++]=arr[i];
-         }
-     }
-          
-           for(int i=0;i<n;i++){
-            arr[i]=temp[i];
-        
+            if(arr[i]!=0){
+                int temp=arr[i];
+                arr[i]=arr[count];
+                arr[count]=temp;
+                count++;
+            }
+        }
     }
 }
-}
+         
