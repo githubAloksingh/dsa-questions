8affa07e886cf22cf7661af6e0e650dd7acbf198
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2023-10-06 00:17:43 +0530

    Update 0287-find-the-duplicate-number.java

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -10,3 +10,20 @@ class Solution {
     return -1;
     }
 }
+
+
+//  O(n^2) Time Limit Exceeded
+class Solution {
+    public int findDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    return nums[i];
+                }
+            }
+        }
+        return 0;
+
+
+    }
+}
