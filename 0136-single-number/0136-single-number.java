6cb3244233c1d6020b0c322d294fc656ff94780e
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2023-10-30 14:14:32 +0530

    Time: 532 ms (5.05%), Space: 44.6 MB (38.39%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+         for(int i=0;i<nums.length;i++){
+            int count=0;
+            
+            for(int j=0;j<nums.length;j++){
+                if(nums[j]==nums[i])
+                    count++;
+            }
+            if (count==1)
+                return nums[i];
+        }
+        return -1;
+        
+    }
+}
