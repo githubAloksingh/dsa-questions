c7ae261df80ea2909a754793c0bd9c384c0ba77e
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2024-01-09 12:22:24 +0530

    Time: 1 ms (95.77%), Space: 44.6 MB (14.97%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> ds=new ArrayList<>();
+        boolean freq[]=new boolean[nums.length];
+        recurPermute(nums,ds,ans,freq);
+        return ans;
+        }
+    public void recurPermute(int []nums,List<Integer>ds,List<List<Integer>>ans,boolean[]freq){
+        if(ds.size()==nums.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(!freq[i]){
+                freq[i]=true;
+                ds.add(nums[i]);
+                recurPermute(nums,ds,ans,freq);
+                ds.remove(ds.size()-1);
+                freq[i]=false;
+            }
+            }
+    }
+}
