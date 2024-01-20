086162d77d226eb98b2091da5ac08ffd7bc80bb8
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2024-01-20 18:14:52 +0530

    Time: 11 ms (28.87%), Space: 44.2 MB (52.12%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> ans = new ArrayList<>();
+        
+        for (int n : nums) {
+            ans.add(n);
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        Collections.sort(ans, (a, b) ->
+            (map.get(a) == map.get(b)) ? b - a : map.get(a) - map.get(b));
+        return ans.stream().mapToInt(i -> i).toArray(); 
+    }
+}
+
+
