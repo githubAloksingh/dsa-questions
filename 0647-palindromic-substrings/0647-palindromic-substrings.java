8914989dc2e601cab3fe355518c20ae3f3228ddd
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2024-02-10 10:39:54 +0530

    Time: 316 ms (7.35%), Space: 45.1 MB (24.39%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=i+1;j<=s.length();j++){
+                if(isPalindrome(s.substring(i,j))){
+                    count++;
+                }
+            }
+        }
+        return count;
+        
+    }
+    private boolean isPalindrome(String s){
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
