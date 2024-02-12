4a34075c23272db4e82a1aca95fe72f5fa56116b
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2024-02-12 09:51:30 +0530

    Time: 11 ms (36.34%), Space: 48.7 MB (85.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,52 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // int n=nums.length;
-        // for(int i=0;i<nums.length;i++){
-        //     int count=0;
-        //     for(int j=0;j<nums.length;j++){
-        //         if(nums[i]==nums[j]){
-        //             count++;
-        //          }
-        // }
-        //     if(count>n/2)
-        //         return nums[i];
-        // }
-        // return -1;
-        
-        
-        
-//         HashMap<Integer, Integer> map = new HashMap<>();
-//          int n=nums.length;
-//         for (int i = 0; i < n; i++) {
-//         int value = map.getOrDefault(nums[i], 0);
-//             map.put(nums[i], value+1);
-//         }
-//         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-//             if (entry.getValue() > (n / 2)) {
-//                 return entry.getKey();
-        
-//             }
-//         }
-
-//         return -1;
-        
-        int n=nums.length;
-        int count=0;
-        int el=nums[0];
-        for(int i=0;i<n;i++){
-            if(count==0){
-                count=1;
-                el=nums[i];
-            }
-            else if(nums[i]==el)
-                count++;
-            else
-                count--;
+        HashMap<Integer,Integer> map= new HashMap<>();
+        for(int num: nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-        return el;
+        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+            int value=entry.getValue();
+            int key=entry.getKey();
+            if(value>nums.length/2){
+                return key;
+            }
+        }
+        return 0;
+        
     }
 }
-
-
-
-  
