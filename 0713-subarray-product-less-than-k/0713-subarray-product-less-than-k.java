fd0590e4cc43cdf01d32249f130a3b23d0310757
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2024-03-27 11:49:23 +0530

    Update 0713-subarray-product-less-than-k.java

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,19 +1,3 @@
-// class Solution {
-//     public int numSubarrayProductLessThanK(int[] nums, int k) {
-//         int count=0;
-//         for(int i=0;i<nums.length;i++){
-//             int product=1;
-//             for(int j=i;j<nums.length;j++){
-//                 product*=nums[j];
-                
-//                 if(product<k){
-//                     count++;
-//                 }
-//             }
-//         }
-//         return count;
-//     }
-// }
 
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
