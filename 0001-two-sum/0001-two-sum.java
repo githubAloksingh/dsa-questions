9a78af79ba512e96bf8ee8a86206eb997a98fc7a
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2024-04-05 21:19:39 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,7 +3,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) {
-                    return new int[] { i, j };
+                    return new int[] { i,  j };
                 }
             }
         }
