9176db4c4f214a3d6f386cbc3897fa1bcc11082a
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2024-05-06 21:14:26 +0530

    Time: 41 ms (27.93%), Space: 64.9 MB (75.38%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode current = head;
+        while (current != null) {
+            stack.push(current);
+            current = current.next;
+        }
+
+        current = stack.pop();
+        int maximum = current.val;
+        ListNode resultList = new ListNode(maximum);
+        while (!stack.isEmpty()) {
+            current = stack.pop();
+    
+            if (current.val < maximum) {
+                continue;
+            }
+            else {
+                ListNode newNode = new ListNode(current.val);
+                newNode.next = resultList;
+                resultList = newNode;
+                maximum = current.val;
+            }
+        }
+
+        return resultList;
+    }
+}
