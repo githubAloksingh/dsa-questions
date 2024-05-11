cf14d76bad9197a3805aa0edf4e86f20e9fb2971
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2024-05-11 22:55:33 +0530

    Time: 38 ms (7.91%), Space: 46.2 MB (11.86%) - LeetHub

---
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.java
@@ -0,0 +1,34 @@
+class Solution {
+    public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+        int n = quality.length;
+        double totalCost = Double.MAX_VALUE;
+        double currentTotalQuality = 0;
+        List<Pair<Double, Integer>> wageToQualityRatio = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            wageToQualityRatio.add(
+                    new Pair<>((double) wage[i] / quality[i], quality[i]));
+        }
+
+        Collections.sort(wageToQualityRatio,
+                Comparator.comparingDouble(Pair::getKey));
+
+        PriorityQueue<Integer> highestQualityWorkers = new PriorityQueue<>(
+                Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            highestQualityWorkers.add(wageToQualityRatio.get(i).getValue());
+            currentTotalQuality += wageToQualityRatio.get(i).getValue();
+
+            if (highestQualityWorkers.size() > k) {
+                currentTotalQuality -= highestQualityWorkers.poll();
+            }
+
+            if (highestQualityWorkers.size() == k) {
+                totalCost = Math.min(totalCost, currentTotalQuality *
+                        wageToQualityRatio.get(i).getKey());
+            }
+        }
+        return totalCost;
+    }
+}
