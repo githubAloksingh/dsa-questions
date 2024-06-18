59b82a3a3ce75af006779a95c85edb6c82a788ce
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2024-06-18 19:16:35 +0530

    Time: 96 ms (22.94%), Space: 41.1 MB (66.37%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        int n= difficulty.size();
+        int m = worker.size();
+        priority_queue<pair<int,int>> pq;
+        
+        for(int i=0;i<n;i++){
+            int diff=difficulty[i];
+            int prof=profit[i];
+            pq.push({prof, diff});
+        }
+        sort(begin(worker),end(worker), greater<int>());
+        int i=0;
+        int totalProfit=0;
+        while(i<m && !pq.empty()){
+            if(pq.top().second>worker[i]){
+                pq.pop();
+            }
+            else{
+                totalProfit+=pq.top().first;
+                i++;
+            }
+        }
+        return totalProfit;
+    }
+};
