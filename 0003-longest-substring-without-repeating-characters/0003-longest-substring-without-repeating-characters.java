f6a84ca23962e70c5541427e48009dcad9aaac57
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2024-07-04 18:50:27 +0530

    Time: 6 ms (68.34%), Space: 44.9 MB (23.14%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,68 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        int left=0;
-        int maxi=0;
-        for(int right=0;right<s.length();right++){
-            if(map.containsKey(s.charAt(right))){
-                left=Math.max(left,map.get(s.charAt(right))+1);
+        Set<Character> set =new HashSet<>();
+        int i=0, j=0, maxi=0;
+        int n =s.length(); 
+        while(i<n && j<n){
+            if(!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                maxi = Math.max(maxi, j-i);
             }
-            map.put(s.charAt(right),right);
-            maxi=Math.max(maxi,right-left+1);
-            
+            else{
+                set.remove(s.charAt(i++));
+            }
         }
         return maxi;
-        
     }
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         int n = s.length();
-//         int maxSubstringLength = 0;
-//         HashMap<Character, Integer> charIndexMap = new HashMap<>();
-        
-//         for (int right = 0, left = 0; right < n; right++) {
-//             char currentChar = s.charAt(right);
-//             if (charIndexMap.containsKey(currentChar)) {
-               
-//                 left = Math.max(left, charIndexMap.get(currentChar) + 1);
-//             }
-//             charIndexMap.put(currentChar, right);
-//             maxSubstringLength = Math.max(maxSubstringLength, right - left + 1);
-//         }
-        
-//         return maxSubstringLength;
-//     }
-// }
+      
