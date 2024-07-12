84515eb294aa3666e97546ca7d6bd66d60902842
githubAloksingh <133560050+githubAloksingh@users.noreply.github.com>
2024-07-12 15:21:47 +0530

    Time: 1 ms (81.39%), Space: 45.4 MB (53.61%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,22 +1,38 @@
-class Solution {
+public class Solution {
     public void setZeroes(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        boolean rows[]=new boolean[m];
-        boolean columns[] = new boolean[n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    rows[i]=true;
-                    columns[j]=true;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] copy = new int[m][n];
+        
+        // Copy the original matrix into the copy matrix
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                copy[i][j] = matrix[i][j];
+            }
+        }
+        
+        // Traverse the original matrix
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // If an element in the original matrix is 0,
+                // set the corresponding row and column in the copy matrix to 0
+                if (matrix[i][j] == 0) {
+                    // Set the entire row to 0
+                    for (int k = 0; k < n; k++) {
+                        copy[i][k] = 0;
+                    }
+                    // Set the entire column to 0
+                    for (int k = 0; k < m; k++) {
+                        copy[k][j] = 0;
+                    }
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(rows[i] || columns[j]){
-                    matrix[i][j]=0;
-                }
+        
+        // Copy the modified copy matrix back to the original matrix
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = copy[i][j];
             }
         }
     }
