14d6d0ff77e08662b1ce899c9cb26eccfc53f178
Alok Singh <133560050+githubAloksingh@users.noreply.github.com>
2024-07-18 00:12:07 +0530

    Time: 1 ms (97.28%), Space: 44.7 MB (58.32%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
@@ -0,0 +1,47 @@
+class Solution {
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        Set<Integer> toDeleteSet = new HashSet<>();
+        for (int val : to_delete) {
+            toDeleteSet.add(val);
+        }
+        List<TreeNode> forest = new ArrayList<>();
+
+        root = processNode(root, toDeleteSet, forest);
+
+        // If the root is not deleted, add it to the forest
+        if (root != null) {
+            forest.add(root);
+        }
+
+        return forest;
+    }
+
+    private TreeNode processNode(
+        TreeNode node,
+        Set<Integer> toDeleteSet,
+        List<TreeNode> forest
+    ) {
+        if (node == null) {
+            return null;
+        }
+
+        node.left = processNode(node.left, toDeleteSet, forest);
+        node.right = processNode(node.right, toDeleteSet, forest);
+
+        // Node Evaluation: Check if the current node needs to be deleted
+        if (toDeleteSet.contains(node.val)) {
+            // If the node has left or right children, add them to the forest
+            if (node.left != null) {
+                forest.add(node.left);
+            }
+            if (node.right != null) {
+                forest.add(node.right);
+            }
+            // Return null to its parent to delete the current node
+            return null;
+        }
+
+        return node;
+    }
+}
