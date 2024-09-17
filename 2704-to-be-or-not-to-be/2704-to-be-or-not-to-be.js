13258f1e30d8b36c4816267f5d352422c1155dff
Alok Singh <133560050+githubAloksingh@users.noreply.github.com>
2024-09-17 16:25:06 +0530

    Time: 56 ms (30.62%), Space: 48.7 MB (72.48%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe:(val2)=>{
+            if(val===val2) return true;
+            else throw new Error("Not Equal");
+        },
+        
+        notToBe:(val2)=>{
+            if(val!==val2) return true;
+            else throw new Error("Equal");
+        }
+        
+        
+    };
+    
+};
+
+
+
