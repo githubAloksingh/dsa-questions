8e773a0f9a2d64caf25ced67ad7ae1ba02f4511c
Alok Singh <133560050+githubAloksingh@users.noreply.github.com>
2024-09-28 11:14:46 +0530

    Time: 1 ms (98.76%), Space: 43.9 MB (12.23%) - LeetHub

---
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        int maxi=0;
+        Queue<Pair<TreeNode,Integer>> queue = new LinkedList<>();
+        queue.offer(new Pair<>(root,0));
+        while(!queue.isEmpty()){
+            int n=queue.size();
+            int left = queue.peek().getValue();
+           int right=left;
+            
+           
+            for(int i=0;i<n;i++){
+                Pair<TreeNode, Integer> current = queue.poll();
+                TreeNode node = current.getKey();
+                int idx = current.getValue();
+                right=idx;
+                if(node.left!=null){
+                    queue.offer(new Pair<>(node.left,idx*2+1));
+                    
+                }
+                
+                if(node.right!=null){
+                    queue.offer(new Pair<>(node.right,idx*2+2));
+                
+            }
+            }
+             maxi=Math.max(maxi,right-left+1);
+                
+            
+        }
+            return maxi;
+    }}
+
+class Pair<K, V>{
+    private K key;
+    private V value;
+    public Pair(K key, V value){
+        this.key=key;
+        this.value=value;
+    }
+    public K getKey(){
+        return key;
+    }
+    public V getValue(){
+        return value;
+    }
+}
+
+
+
