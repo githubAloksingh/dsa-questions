9b31db6383657300ff1da23091d7fb2c9f8ac100
Alok Singh <133560050+githubAloksingh@users.noreply.github.com>
2025-03-10 22:37:37 +0530

    Update 0014-longest-common-prefix.java

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,7 +4,7 @@ class Solution {
         int n=ref.length();
         for(int i=0;i<n;i++){
             char c=ref.charAt(i);
-            
+             
             for(int j=1;j<strs.length;j++){
                 if(i==strs[j].length() || strs[j].charAt(i)!=c){
                     return ref.substring(0,i);
